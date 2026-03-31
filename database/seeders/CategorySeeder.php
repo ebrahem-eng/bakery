40188b5e1dccef6b2fff55929e819b5c4731c089
Ebrahem Alwish <ebrahemalwish@MacBook-Air-Ebrahem.local>
2026-03-31 21:47:10 +0300

    feat: update category seeder with material metadata

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -10,14 +10,43 @@ class CategorySeeder extends Seeder
     public function run()
     {
         $categories = [
-            'طحين',   // Flour
-            'مازوت', // Diesel
-            'خميرة', // Yeast
-            'ملح',    // Salt
+            [
+                'name' => 'طحين',
+                'unit' => 'kg',
+                'input_mode' => 'bags_weight',
+                'track_in_daily_close' => true,
+            ],
+            [
+                'name' => 'مازوت',
+                'unit' => 'liters',
+                'input_mode' => 'simple_quantity',
+                'track_in_daily_close' => true,
+            ],
+            [
+                'name' => 'خميرة',
+                'unit' => 'molds',
+                'input_mode' => 'cartons_molds',
+                'track_in_daily_close' => true,
+            ],
+            [
+                'name' => 'ملح',
+                'unit' => 'kg',
+                'input_mode' => 'simple_quantity',
+                'track_in_daily_close' => true,
+            ],
+            [
+                'name' => 'أكياس',
+                'unit' => 'kg',
+                'input_mode' => 'simple_quantity',
+                'track_in_daily_close' => false,
+            ],
         ];
 
-        foreach ($categories as $category) {
-            Category::firstOrCreate(['name' => $category]);
+        foreach ($categories as $cat) {
+            Category::updateOrCreate(
+                ['name' => $cat['name']],
+                $cat
+            );
         }
     }
 }
